91430fcc658d7c5ee4d19f6bb5f2ea5ddbf71715
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2021-05-25 21:02:52 +0530

    Add files via upload

---
+++ Assignment MVC/Model/Registration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Models
+{
+    public class Registration
+    {
+
+        [Required(ErrorMessage = "Name is compulsary feild")]  
+        public int? Name { get; set; }
+
+        [Required(ErrorMessage = "Email name is compulsary")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone no is compulsary")]
+        public double? Phone { get; set; }
+
+        [Required(ErrorMessage = "Will Attend is compulsary")]
+        //public bool WillAttend { get; set; }
+
+        public WillAttend Options { get; set; }
+       
+    }
+    public enum WillAttend
+    {
+        Yes,
+        No
+    }
+}
