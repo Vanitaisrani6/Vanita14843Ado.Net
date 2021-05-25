2a6c5c558fde8d877bfe0d2b1b82390859846e7d
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2021-05-25 20:10:59 +0530

    Add files via upload
    GUID Code

---
+++ GUID/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GUID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid guid = new Guid();
+            Console.WriteLine(guid);
+
+            Guid guid1 = Guid.NewGuid();
+            Console.WriteLine(guid1);
+
+            Console.WriteLine(Guid.NewGuid()); //by creating new instance it will create new GUID
+        }
+    }
+}
