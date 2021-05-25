e0c9eb3dae4c3fc27dcdbb9ed68eae7e5af66dd9
Vanitaisrani6 <62104742+Vanitaisrani6@users.noreply.github.com>
2021-05-25 10:19:12 +0530

    Add files via upload

---
+++ Assignment MVC/Controller/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using MVCDemoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemoApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult RegistrationForm()
+        {
+            return View(new Registration());
+        }
+
+        
+        public IActionResult DisplayRegistrationData(Registration registration)
+        {
+
+            return View();
+        }
+
+
+    }
+}
